fb046ad64e88eeacee0de08f37b07466e2d36d52
Igor Aleksanov <popzxc@yandex.ru>
2020-06-26 14:40:36 +0300

    DurationExt: 'as' methods

--- src/duration.rs
+++ src/duration.rs
@@ -1,5 +1,9 @@
 use std::time::Duration;
 
+const SECS_IN_MIN: u64 = 60;
+const SECS_IN_HOUR: u64 = 3600;
+const SECS_IN_DAY: u64 = 3600 * 24;
+
 pub trait DurationExt {
     fn from_minutes(minutes: u64) -> Duration;
     fn from_hours(hours: u64) -> Duration;
@@ -12,22 +16,26 @@ pub trait DurationExt {
     fn add_minutes(self, minutes: u64) -> Duration;
     fn add_hours(self, hours: u64) -> Duration;
     fn add_days(self, days: u64) -> Duration;
+
+    fn as_minutes(&self) -> u64;
+    fn as_hours(&self) -> u64;
+    fn as_days(&self) -> u64;
 }
 
 impl DurationExt for Duration {
     fn from_minutes(minutes: u64) -> Self {
-        let seconds = minutes * 60;
+        let seconds = minutes * SECS_IN_MIN;
         Self::from_secs(seconds)
     }
 
     fn from_hours(hours: u64) -> Self {
-        let seconds: u64 = hours * 3600;
+        let seconds: u64 = hours * SECS_IN_HOUR;
         Self::from_secs(seconds)
     }
 
     fn from_days(days: u64) -> Self {
-        let hours = days * 24;
-        Self::from_hours(hours)
+        let seconds = days * SECS_IN_DAY;
+        Self::from_secs(seconds)
     }
 
     fn add_nanos(self, nanos: u64) -> Self {
@@ -57,6 +65,18 @@ impl DurationExt for Duration {
     fn add_days(self, days: u64) -> Self {
         self + Self::from_days(days)
     }
+
+    fn as_minutes(&self) -> u64 {
+        self.as_secs() / SECS_IN_MIN
+    }
+
+    fn as_hours(&self) -> u64 {
+        self.as_secs() / SECS_IN_HOUR
+    }
+
+    fn as_days(&self) -> u64 {
+        self.as_secs() / SECS_IN_DAY
+    }
 }
 
 #[cfg(test)]
@@ -64,11 +84,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn from_minutes() {
-        const SECS_IN_MIN: u64 = 60;
-
+    fn from_methods() {
+        // Check `from_minutes`
         let test_vector = vec![0, 1, u64::max_value() / SECS_IN_MIN];
-
         for minutes in test_vector {
             let seconds = minutes * SECS_IN_MIN;
             assert_eq!(
@@ -76,26 +94,16 @@ mod tests {
                 Duration::from_secs(seconds)
             );
         }
-    }
 
-    #[test]
-    fn from_hours() {
-        const SECS_IN_HOUR: u64 = 3600;
-
+        // Check `from_hours`
         let test_vector = vec![0, 1, u64::max_value() / SECS_IN_HOUR];
-
         for hours in test_vector {
             let seconds = hours * SECS_IN_HOUR;
             assert_eq!(Duration::from_hours(hours), Duration::from_secs(seconds));
         }
-    }
 
-    #[test]
-    fn from_days() {
-        const SECS_IN_DAY: u64 = 3600 * 24;
-
+        // Check `from_days`
         let test_vector = vec![0, 1, u64::max_value() / SECS_IN_DAY];
-
         for days in test_vector {
             let seconds = days * SECS_IN_DAY;
             assert_eq!(Duration::from_days(days), Duration::from_secs(seconds));
@@ -114,10 +122,25 @@ mod tests {
             .add_days(1);
 
         let expected_duration = Duration::new(
-            1 * (3600 * 24) + 1 * 3600 + 1 * 60 + 1,
+            1 * SECS_IN_DAY + 1 * SECS_IN_HOUR + 1 * SECS_IN_MIN + 1,
             1 * 1_000_000 + 1 * 1_000 + 1,
         );
 
         assert_eq!(duration, expected_duration);
+    }
+
+    #[test]
+    fn as_methods() {
+        let test_vector = vec![0, SECS_IN_MIN, SECS_IN_HOUR, SECS_IN_DAY];
+
+        for seconds in test_vector {
+            for seconds in &[seconds, seconds + 1, seconds * 2, seconds * 2 + 1] {
+                let duration = Duration::from_secs(*seconds);
+
+                assert_eq!(duration.as_minutes(), duration.as_secs() / SECS_IN_MIN);
+                assert_eq!(duration.as_hours(), duration.as_secs() / SECS_IN_HOUR);
+                assert_eq!(duration.as_days(), duration.as_secs() / SECS_IN_DAY);
+            }
+        }
     }
 }
