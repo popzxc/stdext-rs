1af08dcdfd002f51215caba4d9020a94deaeaa28
Igor Aleksanov <popzxc@yandex.ru>
2020-06-26 15:03:26 +0300

    OptionExt: Add 'combine_with'

--- src/option.rs
+++ src/option.rs
@@ -1,5 +1,9 @@
 pub trait OptionExt<T> {
     fn combine<U>(self, other: Option<U>) -> Option<(T, U)>;
+
+    fn combine_with<U, F, R>(self, other: Option<U>, f: F) -> Option<R>
+    where
+        F: FnOnce(T, U) -> R;
 }
 
 impl<T> OptionExt<T> for Option<T> {
@@ -8,6 +12,13 @@ impl<T> OptionExt<T> for Option<T> {
             (Some(left), Some(right)) => Some((left, right)),
             _ => None,
         }
+    }
+
+    fn combine_with<U, F, R>(self, other: Option<U>, f: F) -> Option<R>
+    where
+        F: FnOnce(T, U) -> R,
+    {
+        self.combine(other).map(|(l, r)| f(l, r))
     }
 }
 
@@ -27,6 +38,25 @@ mod tests {
 
         for (left, right, expected) in test_vector {
             assert_eq!(left.combine(right), expected);
+        }
+    }
+
+    #[test]
+    fn combine_with() {
+        fn f(l: i32, r: i32) -> i32 {
+            l + r
+        };
+
+        // Test vector of (left, right, expected) values.
+        let test_vector = vec![
+            (Some(1), Some(2), Some(3)),
+            (Some(1), None, None),
+            (None, Some(2), None),
+            (None, None, None),
+        ];
+
+        for (left, right, expected) in test_vector {
+            assert_eq!(left.combine_with(right, f), expected);
         }
     }
 }
