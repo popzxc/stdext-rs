41b49da534bf2cf6c055794b2c59bba92667e9d1
Igor Aleksanov <popzxc@yandex.ru>
2020-06-26 14:32:31 +0300

    DurationExt: 'add' methods

--- src/duration.rs
+++ src/duration.rs
@@ -1,13 +1,21 @@
 use std::time::Duration;
 
 pub trait DurationExt {
-    fn from_mins(minutes: u64) -> Duration;
+    fn from_minutes(minutes: u64) -> Duration;
     fn from_hours(hours: u64) -> Duration;
     fn from_days(days: u64) -> Duration;
+
+    fn add_nanos(self, nanos: u64) -> Duration;
+    fn add_micros(self, micros: u64) -> Duration;
+    fn add_millis(self, millis: u64) -> Duration;
+    fn add_secs(self, seconds: u64) -> Duration;
+    fn add_minutes(self, minutes: u64) -> Duration;
+    fn add_hours(self, hours: u64) -> Duration;
+    fn add_days(self, days: u64) -> Duration;
 }
 
 impl DurationExt for Duration {
-    fn from_mins(minutes: u64) -> Self {
+    fn from_minutes(minutes: u64) -> Self {
         let seconds = minutes * 60;
         Self::from_secs(seconds)
     }
@@ -21,6 +29,34 @@ impl DurationExt for Duration {
         let hours = days * 24;
         Self::from_hours(hours)
     }
+
+    fn add_nanos(self, nanos: u64) -> Self {
+        self + Self::from_nanos(nanos)
+    }
+
+    fn add_micros(self, micros: u64) -> Self {
+        self + Self::from_micros(micros)
+    }
+
+    fn add_millis(self, millis: u64) -> Self {
+        self + Self::from_millis(millis)
+    }
+
+    fn add_secs(self, seconds: u64) -> Self {
+        self + Self::from_secs(seconds)
+    }
+
+    fn add_minutes(self, minutes: u64) -> Self {
+        self + Self::from_minutes(minutes)
+    }
+
+    fn add_hours(self, hours: u64) -> Self {
+        self + Self::from_hours(hours)
+    }
+
+    fn add_days(self, days: u64) -> Self {
+        self + Self::from_days(days)
+    }
 }
 
 #[cfg(test)]
@@ -28,14 +64,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn from_mins() {
+    fn from_minutes() {
         const SECS_IN_MIN: u64 = 60;
 
         let test_vector = vec![0, 1, u64::max_value() / SECS_IN_MIN];
 
         for minutes in test_vector {
             let seconds = minutes * SECS_IN_MIN;
-            assert_eq!(Duration::from_mins(minutes), Duration::from_secs(seconds));
+            assert_eq!(
+                Duration::from_minutes(minutes),
+                Duration::from_secs(seconds)
+            );
         }
     }
 
@@ -61,5 +100,24 @@ mod tests {
             let seconds = days * SECS_IN_DAY;
             assert_eq!(Duration::from_days(days), Duration::from_secs(seconds));
         }
+    }
+
+    #[test]
+    fn add_methods() {
+        let duration = Duration::default()
+            .add_nanos(1)
+            .add_micros(1)
+            .add_millis(1)
+            .add_secs(1)
+            .add_minutes(1)
+            .add_hours(1)
+            .add_days(1);
+
+        let expected_duration = Duration::new(
+            1 * (3600 * 24) + 1 * 3600 + 1 * 60 + 1,
+            1 * 1_000_000 + 1 * 1_000 + 1,
+        );
+
+        assert_eq!(duration, expected_duration);
     }
 }
