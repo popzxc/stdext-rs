22de0ba1f65bfbfe5a0602757bee6c01c798c3f3
Igor Aleksanov <popzxc@yandex.ru>
2020-06-26 16:14:41 +0300

    RwLockExt: Add trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 pub mod duration;
 pub mod option;
 pub mod result;
+pub mod sync;
 pub mod vec;
 
 pub mod prelude {
     pub use crate::duration::*;
     pub use crate::option::*;
     pub use crate::result::*;
+    pub use crate::sync::rw_lock::*;
     pub use crate::vec::*;
 }
